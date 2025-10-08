8d36b664d5738dabdff7d72ff5425c152160bd6f
Sukhatai09 <sukhatai7331@gmail.com>
2025-10-08 17:08:03 +0700

    ปรับ Sidebar  ให้ขึ้นhover  ในรายการที่เลือก

--- frontend/src/app/(main)/manage/timeline/page.tsx
+++ frontend/src/app/(main)/manage/timeline/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-       ครีมสวยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยยย
+       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam vel cupiditate, ab nisi, magni molestiae asperiores, quasi similique consequatur libero autem voluptatibus dolore minus quidem eum ea natus vero sit?
     </div>
   )
 }
