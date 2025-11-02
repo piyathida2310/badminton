7a0b8a448a471ddf8b582d1d770d0b75b586db3b
Sukhatai09 <sukhatai7331@gmail.com>
2025-11-02 19:50:03 +0700

    จัดการ buttom

--- frontend/src/app/(main)/manage/bracket/page.tsx
+++ frontend/src/app/(main)/manage/bracket/page.tsx
@@ -8,9 +8,9 @@ const page = () => {
   const [round, setRound] = useState<number>(16);
   return (
     <div className="bg-[#f9f9f0] overflow-y-auto">
-      <button className='bg-pink-400 w-24 h24 rounded-3xl mr-5' onClick={()=>setRound(16)}>16</button>
+      {/* <button className='bg-pink-400 w-24 h24 rounded-3xl mr-5' onClick={()=>setRound(16)}>16</button> */}
       {/* <button className='bg-pink-400 w-24 h24 rounded-3xl mr-5' onClick={()=>setRound(24)}>24</button> */}
-      <button className='bg-pink-400 w-24 h24 rounded-3xl' onClick={()=>setRound(32)}>32</button>
+      {/* <button className='bg-pink-400 w-24 h24 rounded-3xl' onClick={()=>setRound(32)}>32</button> */}
       {round === 16 ? (
         <div>
           <SixteenBracket level="บน" />
